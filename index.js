4f1fec4ffb16c65011206047f83d3d58c599cd22
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-01 19:48:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  //In order for this to work, we need two equal sized arrays.
+  if(musicians.length != instruments.length){
+    return null;
+  }
+  
+  let result = [];
+  
+  for(var i = 0; i < musicians.length; i++){
+    result.append(musicians[i] + " plays " + instruments[i]);
+  }
+  
+}
